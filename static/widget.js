f2f15a37cdc2c2be1dd86ec20e736194432bf173
Ivan Miric <imiric@gmail.com>
2014-10-23 21:46:36 +0200

    Enable scrolling within iframe
    So the images can be seen properly.

--- static/widget.js
+++ static/widget.js
@@ -6,6 +6,5 @@
   iframe.setAttribute('width', '300');
   iframe.setAttribute('height', '240');
   iframe.setAttribute('frameborder', '0');
-  iframe.setAttribute('scrolling', 'no');
   el.parentNode.replaceChild(iframe, el);
 })();
