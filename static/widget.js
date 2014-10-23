b6663121f9d6e36e7e479b197e39ff2b9cb0f70d
Ivan Miric <imiric@gmail.com>
2014-10-23 23:47:44 +0200

    Add id to widget's iframe and fix src URL

--- static/widget.js
+++ static/widget.js
@@ -2,7 +2,8 @@
   var el, iframe;
   el = document.getElementById('widget');
   iframe = document.createElement('iframe');
-  iframe.setAttribute('src', el.href + "/widget");
+  iframe.setAttribute('id', 'widget');
+  iframe.setAttribute('src', el.href + 'widget/');
   iframe.setAttribute('width', '300');
   iframe.setAttribute('height', '240');
   iframe.setAttribute('frameborder', '0');
